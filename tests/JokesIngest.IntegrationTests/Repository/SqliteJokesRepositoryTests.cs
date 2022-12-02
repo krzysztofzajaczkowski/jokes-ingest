ee5c6de1e7acc9ef6fb6060f30778731b6ffe574
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2022-12-02 23:05:48 +0100

    #3 Handle duplicate jokes in database

--- tests/JokesIngest.IntegrationTests/Repository/SqliteJokesRepositoryTests.cs
+++ tests/JokesIngest.IntegrationTests/Repository/SqliteJokesRepositoryTests.cs
@@ -23,7 +23,6 @@ public class SqliteJokesRepositoryTests
     };
 
     private Because of = async () => await repository.SaveJokes(jokes.ToAsyncEnumerable());
-    
 
     class When_empty_jokes_collection
     {
@@ -50,5 +49,51 @@ public class SqliteJokesRepositoryTests
             var savedJokes = await repository.GetJokesAsync();
             savedJokes.ShouldEqual(jokes);
         };
+    }
+
+    class When_adding_multiple_occurrences_of_same_joke
+    {
+        class When_all_occurrences_within_same_transaction
+        {
+            private Establish ctx = () => jokes = new[]
+            {
+                New.Joke(id: "1", iconUrl: "id1-icon-url", url: "id1-url", value: "A joke"),
+                New.Joke(id: "2", iconUrl: "id2-icon-url", url: "id2-url", value: "A joke"),
+            };
+
+            private It should_save_only_first_joke = async () =>
+            {
+                var savedJokes = await repository.GetJokesAsync();
+                savedJokes.ShouldEqual(new[]
+                {
+                    New.Joke(id: "1", iconUrl: "id1-icon-url", url: "id1-url", value: "A joke"),
+                });
+            };
+        }
+
+        class When_occurrences_in_different_transactions
+        {
+            private Establish ctx = async () =>
+            {
+                await repository.SaveJokes(new[]
+                {
+                    New.Joke(id: "1", iconUrl: "id1-icon-url", url: "id1-url", value: "A joke"),
+                }.ToAsyncEnumerable());
+
+                jokes = new[]
+                {
+                    New.Joke(id: "2", iconUrl: "id2-icon-url", url: "id2-url", value: "A joke"),
+                };
+            };
+
+            private It should_not_save_second_joke = async () =>
+            {
+                var savedJokes = await repository.GetJokesAsync();
+                savedJokes.ShouldEqual(new[]
+                {
+                    New.Joke(id: "1", iconUrl: "id1-icon-url", url: "id1-url", value: "A joke"),
+                });
+            };
+        }
     }
 }
