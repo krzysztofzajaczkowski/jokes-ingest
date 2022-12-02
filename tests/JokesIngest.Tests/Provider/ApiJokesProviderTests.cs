2b57c74b615bd6a76c51308681386b420ba30bdd
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2022-12-02 19:47:36 +0100

    #2 Add jokes provider that consumes API

---
+++ tests/JokesIngest.Tests/Provider/ApiJokesProviderTests.cs
@@ -0,0 +1,91 @@
+﻿using JokesIngest.Model;
+using JokesIngest.Provider;
+using JokesIngest.Tests.Utils;
+using Machine.Fakes;
+using Machine.Specifications;
+
+// ReSharper disable InconsistentNaming
+// ReSharper disable ArrangeTypeMemberModifiers
+
+namespace JokesIngest.Tests.Provider;
+
+[Subject(typeof(ApiJokesProvider))]
+public class ApiJokesProviderTests : WithSubject<ApiJokesProvider>
+{
+    const int BatchSize = 2;
+
+    #region SetupMethods
+
+    static HttpClient SetupClient(HttpMessageHandler handler)
+    {
+        var client = new HttpClient(handler)
+        {
+            BaseAddress = new Uri("http://localhost")
+        };
+        
+        return client;
+    }
+
+    static void SetupProvider(HttpMessageHandler messageHandlerMock)
+    {
+        Configure(x => x.For<HttpClient>()
+            .Use(SetupClient(messageHandlerMock)));
+
+        Configure(x => x.For<JokesProviderConfiguration>()
+            .Use(new JokesProviderConfiguration(BatchSize, "jokes/random")));
+    }
+
+    #endregion
+
+    class When_all_jokes_results_success
+    {
+        static IEnumerable<Joke> jokes;
+
+        Establish ctx = () =>
+            SetupProvider(HttpMessageHandlerMock.SetupSuccessResultsForJokes(new[]
+            {
+                New.Joke(value: "A joke"),
+                New.Joke(value: "Another joke"),
+                New.Joke(value: "Next Chuck joke"),
+            }).Object);
+
+        Because of = () => jokes = Subject.GetJokesAsync().ToEnumerable().ToArray();
+
+        It should_get_results_successfully = () => jokes.ShouldEqual(new[]
+        {
+            New.Joke(value: "A joke"),
+            New.Joke(value: "Another joke"),
+        });
+
+        It should_get_correct_batch_size = () => jokes.Count().ShouldEqual(BatchSize);
+    }
+
+    class When_failure
+    {
+        static Exception exception;
+
+        Because of = () => exception = Catch.Exception(() => Subject.GetJokesAsync().ToEnumerable().ToArray());
+
+        class When_first_joke_result_s_failure
+        {
+            Establish ctx = () => SetupProvider(HttpMessageHandlerMock.SetupFailureResult().Object);
+
+            It should_throw_http_request_exception =
+                () => exception.ShouldBeOfExactType<HttpRequestException>();
+        }
+
+        class When_middle_joke_result_is_failure
+        {
+            Establish ctx = () =>
+                SetupProvider(HttpMessageHandlerMock.SetupSuccessResultsForJokesWithErrorInTheMiddle(new[]
+                {
+                    New.Joke(value: "A joke"),
+                    New.Joke(value: "Another joke"),
+                    New.Joke(value: "Next Chuck joke"),
+                }).Object);
+
+            It should_throw_http_request_exception =
+                () => exception.ShouldBeOfExactType<HttpRequestException>();
+        }
+    }
+}
---
+++ tests/JokesIngest.Tests/Utils/HttpMessageHandlerMock.cs
@@ -0,0 +1,71 @@
+﻿using System.Linq.Expressions;
+using JokesIngest.Model;
+using Moq;
+using Newtonsoft.Json;
+using System.Net;
+using Moq.Protected;
+
+namespace JokesIngest.Tests.Utils;
+
+internal static class HttpMessageHandlerMock
+{
+    private interface IHttpMessageHandlerProtectedMembers
+    {
+        Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken);
+    }
+
+    private static IProtectedAsMock<HttpMessageHandler, IHttpMessageHandlerProtectedMembers>
+        AsProtected(this Mock<HttpMessageHandler> mock) => mock.Protected().As<IHttpMessageHandlerProtectedMembers>();
+
+    private static Expression<Func<IHttpMessageHandlerProtectedMembers, Task<HttpResponseMessage>>>
+        SendAsyncExpression => x =>
+            x.SendAsync(It.IsAny<HttpRequestMessage>(), It.IsAny<CancellationToken>());
+
+    private static HttpResponseMessage CreateHttpResponseMessage(HttpStatusCode code = HttpStatusCode.OK, Joke joke = null)
+    {
+        return new HttpResponseMessage
+        {
+            StatusCode = code,
+            Content = (joke == null ? default : new StringContent(JsonConvert.SerializeObject(joke)))!
+        };
+    }
+
+    public static Mock<HttpMessageHandler> SetupFailureResult()
+    {
+        var mockMessageHandler = new Mock<HttpMessageHandler>();
+        mockMessageHandler.AsProtected()
+            .Setup(SendAsyncExpression)
+            .ReturnsAsync(CreateHttpResponseMessage(HttpStatusCode.BadGateway));
+
+        return mockMessageHandler;
+    }
+
+    public static Mock<HttpMessageHandler> SetupSuccessResultsForJokes(IEnumerable<Joke> jokes)
+    {
+        var mockMessageHandler = new Mock<HttpMessageHandler>();
+        var sequenceSetup = mockMessageHandler.AsProtected()
+            .SetupSequence(SendAsyncExpression);
+        foreach (var joke in jokes)
+        {
+            sequenceSetup.ReturnsAsync(CreateHttpResponseMessage(joke: joke));
+        }
+
+        return mockMessageHandler;
+    }
+
+    public static Mock<HttpMessageHandler> SetupSuccessResultsForJokesWithErrorInTheMiddle(IEnumerable<Joke> jokes)
+    {
+        var results = jokes.Select(joke => CreateHttpResponseMessage(joke: joke)).ToList();
+        results.Insert(results.Count / 2, CreateHttpResponseMessage(HttpStatusCode.BadGateway));
+
+        var mockMessageHandler = new Mock<HttpMessageHandler>();
+        var sequenceSetup = mockMessageHandler.AsProtected()
+            .SetupSequence(SendAsyncExpression);
+        foreach (var result in results)
+        {
+            sequenceSetup.ReturnsAsync(result);
+        }
+
+        return mockMessageHandler;
+    }
+}
