37062afb40a9d2b74283b4ee93e340ec7d89489d
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2022-12-02 17:19:52 +0100

    #1 Add Regex joke filter

---
+++ src/JokesIngest/Filters/RegexFilter.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+using JokesIngest.Model;
+
+namespace JokesIngest.Filters
+{
+    public class RegexFilter : IJokeFilter
+    {
+        private readonly string _pattern;
+
+        public RegexFilter(string pattern)
+        {
+            _pattern = pattern;
+        }
+
+        public bool SatisfiedBy(Joke joke)
+        {
+            return Regex.IsMatch(joke.Text, _pattern);
+        }
+    }
+}
