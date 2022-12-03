d5bf0817eb39c9a030abd78c935b6dc3cda38b0a
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2022-12-03 14:07:38 +0100

    #4 Add error handling for Http and Sql exceptions

---
+++ src/JokesIngest.Function/Infrastructure/Middleware/ExceptionHandler.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Middleware;
+
+namespace JokesIngest.Function.Infrastructure.Middleware;
+
+public abstract class ExceptionHandler<TException> : IFunctionsWorkerMiddleware where TException : Exception
+{
+    public abstract Task Handle(FunctionContext context, TException exception);
+
+    public async Task Invoke(FunctionContext context, FunctionExecutionDelegate next)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (TException exception)
+        {
+            await Handle(context, exception);
+        }
+    }
+}
---
+++ src/JokesIngest.Function/Infrastructure/Middleware/SqlExceptionHandler.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.Logging;
+
+namespace JokesIngest.Function.Infrastructure.Middleware;
+
+public class SqlExceptionHandler : ExceptionHandler<SqliteException>
+{
+    private readonly ILogger<SqlExceptionHandler> _logger;
+
+    public SqlExceptionHandler(ILogger<SqlExceptionHandler> logger)
+    {
+        _logger = logger;
+    }
+
+    public override Task Handle(FunctionContext context, SqliteException exception)
+    {
+        _logger.LogError(
+            "Error thrown while ingesting jokes into sqlite database with message: {message}. " +
+            "(Exception error code: {errorCode}, InvocationId: {invocationId})",
+            exception.Message, exception.SqliteErrorCode, context.InvocationId);
+
+        return Task.CompletedTask;
+    }
+}
